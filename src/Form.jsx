0abb7f9319025531cd1893ed6767110d01754c07
isLalitActive <lalit.postemails@gmail.com>
2024-07-12 01:04:48 +0530

    Implemented form comp using checkbox and dropdown

--- src/Form.jsx
+++ src/Form.jsx
@@ -4,13 +4,15 @@ const Form = () => {
   const [formData, setFormData] = useState({
     firstname: '',
     lastname: '',
+    age: '',
+    isMarried: false
   });
 
   const formDataHandler = (e) => {
-    const { name, value } = e.target;
+    const { name, value, type, checked } = e.target;
     setFormData({
-      // ...formData,
-      [name]: value,
+      ...formData,
+      [name]: type === "checkbox" ? checked : value,
     });
   };
 
@@ -35,12 +37,23 @@ const Form = () => {
             onChange={formDataHandler}
           />
         </div>
+        <div>
+          <select name="age" id="ageId" onChange={formDataHandler}>
+            <option value="select your age">select your age</option>
+            <option value="31">31</option>
+            <option value="32">32</option>
+            <option value="33">33</option>
+          </select>
+        </div>
+        <div>
+          <input type="checkbox" name="isMarried" checked={formData.isMarried} onChange={formDataHandler}/>
+        </div>
       </form>
       <div>
-        <h3>
-          {formData.firstname}
-          {formData.lastname}
-        </h3>
+        <h3>{formData.firstname}</h3>
+        <h3>{formData.lastname}</h3>
+        <h3>{formData.age}</h3>
+        <h3>Is Married: {formData.isMarried ? "Yes" : "No"}</h3>
       </div>
     </div>
   );
